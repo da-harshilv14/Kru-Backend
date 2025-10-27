d07d55de5b2b0cb8a8e9471cb507fcd4adbdc99d
da-harshilv14 <202301208@dau.ac.in>
2025-10-28 01:41:25 +0530

    Update wsgi.py

--- back/wsgi.py
+++ back/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'back.settings')
 application = get_wsgi_application()
 
